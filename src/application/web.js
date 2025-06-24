f4b72e64dccfcaa47319b8af38e2584e517a88ba
MochamadLukyAdithia <232410102049@mail.unej.ac.id>
2025-06-24 21:18:49 +0700

    feat : update tetsing web

--- src/application/web.js
+++ src/application/web.js
@@ -1,21 +1,22 @@
-// import express from "express";
-// import {publicRouter} from "../route/public-api.js";
-// import {errorMiddleware} from "../middleware/error-middleware.js";
-// import {userRouter} from "../route/api.js";
-
-// export const web = express();
-// web.use(express.json());
-// web.use(publicRouter);
-// web.use(userRouter);
-// web.use(errorMiddleware);
-
-// src/application/web.js
 import express from "express";
-const app = express();
+import { publicRouter } from "../route/public-api.js";
+import { userRouter } from "../route/api.js";
+import { errorMiddleware } from "../middleware/error-middleware.js";
 
-// setup routes
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+export const web = express();
+web.use(express.json());
+web.use(publicRouter);
+web.use(userRouter);
+web.use(errorMiddleware);
 
-export const web = app;
+
+// // src/application/web.js
+// import express from "express";
+// const app = express();
+
+// // setup routes
+// app.get("/", (req, res) => {
+//   res.send("Hello World");
+// });
+
+// export const web = app;
